aea98eb11714a618bc9263e5d27e61fb7f173664
Rania Widijanto <widijanto@shibe.local>
2020-11-22 20:47:32 +0700

    Added year party & make a wish

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/resonance2020/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/resonance2020/MainActivity.java
@@ -2,7 +2,11 @@ package id.ac.ui.cs.mobileprogramming.resonance2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import id.ac.ui.cs.mobileprogramming.resonance2020.ui.main.MainFragment;
 
@@ -12,10 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        Button part1_button = (Button) findViewById(R.id.button1);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
+
+        part1_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "PAST // FUTURE", Toast.LENGTH_LONG).show();
+                openPart1();
+            }
+        });
+    }
+
+    public void openPart1() {
+        Intent intent = new Intent(this, FullscreenActivity.class);
+        startActivity(intent);
     }
 }
